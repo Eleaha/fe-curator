03b2ade3e34681b11accf7aa9ba3b6eebae68aae
Eleaha <ellieharrington8@gmail.com>
2025-01-27 18:22:42 +0000

    add styling to error page

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -3,7 +3,7 @@ import { ErrorProps } from "../../interfaces";
 
 export const ErrorPage = ({ errorMessage, errorCode }: ErrorProps) => {
 	return (
-		<main className="page">
+		<main className="page" id="error-page">
 			<h1>Uh oh! Something's gone wrong.</h1>
 			<h2>{errorMessage}</h2>
 			<h1>{errorCode}</h1>
